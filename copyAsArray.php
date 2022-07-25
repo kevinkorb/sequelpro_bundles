73e4cf3495de634ea7939a45ea713479678bf0d9
Kevin Korb <kevin@echo19.com>
2022-07-25 15:56:41 -0500

    Fixed issue with line-breaks, and fixed NULLs

--- copyAsArray.php
+++ copyAsArray.php
@@ -1,8 +1,11 @@
-#!/usr/bin/env php
+#!/usr/bin/env /usr/local/bin/php
 <?php
 $stdIn = fopen("php://stdin", "r");
 $result = [];
 while($row = fgetcsv($stdIn, 0)) {
+	foreach($row AS $k => $v){
+		$row[$k] = strtr( $v, array(  "\n" => "\\n",  "\r" => "\\r"  ));
+	}
 	array_push($result, $row);
 }
 $columns = array_shift($result);
@@ -12,6 +15,6 @@ if ($result) {
 	}
 }
 
-$cmd = 'echo '.escapeshellarg(var_export($result, true)).' | __CF_USER_TEXT_ENCODING='.posix_getuid().':0x8000100:0x8000100 pbcopy';
+$cmd = 'echo '.escapeshellarg(str_replace("'NULL'", 'NULL', var_export($result, true))).' | __CF_USER_TEXT_ENCODING='.posix_getuid().':0x8000100:0x8000100 pbcopy';
 shell_exec($cmd);
 
